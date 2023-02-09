2aecf458a93253b9da6379cc0e1909917b8d5fe1
yumemiku <danielat97@hotmail.com>
2023-02-09 23:41:55 +0000

    deleted somethig

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,6 @@ export default function Home() {
     <>
     <h1 className="text-2xl text-center my-4 text-sky-400">Daniela Sofia Sismey</h1>
     <h2> Current Projects - watch this space!</h2>
-    <Link href="./kelvin.html">Kelvin temperature calculator</Link>
     </>
   )
 }
