5badf48b56132f165dff0ee117cc49e84b747b8e
yumemiku <danielat97@hotmail.com>
2023-02-09 22:48:56 +0000

    added kelvin converter and tried to add a link

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,8 @@ export default function Home() {
   return (
     <>
     <h1 className="text-2xl text-center my-4 text-sky-400">Daniela Sofia Sismey</h1>
+    <h2> Current Projects (watch this space!)</h2>
+    <Link href="./kelvin.html">Kelvin temperaturte calculator</Link>
     </>
   )
 }
