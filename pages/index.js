5e408b2e37ae0cbf6a00d7abaa197cf674938f56
yumemiku <danielat97@hotmail.com>
2023-02-09 22:55:41 +0000

    changed some more spelling

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <>
     <h1 className="text-2xl text-center my-4 text-sky-400">Daniela Sofia Sismey</h1>
-    <h2> Current Projects watch this space!</h2>
+    <h2> Current Projects - watch this space!</h2>
     <Link href="./kelvin.html">Kelvin temperature calculator</Link>
     </>
   )
